ac69ddaf6fcaff78cdb91a12a74079455fa8079f
egorystuff <mr.yankovec@gmail.com>
2022-12-11 20:02:24 +0300

    clicks

--- InterProg/InterProg.js
+++ InterProg/InterProg.js
@@ -1,11 +1,11 @@
-//Открываем диалог с задержкой 10 сек
+//Открываем диалог с задержкой 100 сек
 var timeUp = function () {
   alert('Время вышло!');
 };
 
 setTimeout(timeUp, 100000);
 
-//таймер отсчета с шагом 10 сек
+//таймер отсчета с шагом 1 сек
 var counter = 1;
 var printMessage = function () {
   console.log('Ты смотришь на монитору уже ' + counter + ' секунд');
@@ -16,6 +16,8 @@ var time = setInterval(printMessage, 1000);
 //Подвижный текст
 var position = 'вправо';
 var offset = 0;
+var speed = 10;
+var clicks = 0;
 
 // $('#heading').offset({ left: offset, top: offset });
 
@@ -50,8 +52,17 @@ var moveHeading = function () {
     }
   }
 };
-var intervalId = setInterval(moveHeading, 5);
+var intervalId = setInterval(moveHeading, speed);
 
-$('#heading').click(function (event) {
+$('#heading').click(function () {
   clearInterval(intervalId);
+  speed /= 2;
+  clicks++;
+
+  if (clicks > 10) {
+    clearInterval(intervalId);
+    $('#heading').text('Докликался!');
+  } else {
+    intervalId = setInterval(moveHeading, speed);
+  }
 });
