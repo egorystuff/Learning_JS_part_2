e66470ffe2d092df036172c0f37831e114ddd6d1
egorystuff <mr.yankovec@gmail.com>
2022-12-12 20:56:23 +0300

    Update oop.js

--- OOP/oop.js
+++ OOP/oop.js
@@ -4,10 +4,23 @@ var dog = {
   isAvesome: true,
 };
 
+//Точечная нотация. Добавляет новый ключ к объекту
 dog.age = 6;
 
+//метод 1. Добавляется к объекту dog через точ нотацию
 dog.bark = function () {
   console.log('ГАВ-ГАВ я ПЕС по имени ' + this.name + ' !');
 };
 
-console.log(dog.bark());
+//метод 2. присваиваем объекту cat
+var speak = function () {
+  console.log(this.sound + '! Меня зовут ' + this.name + ' !');
+};
+
+var cat = {
+  sound: 'МЯУ МЯУ',
+  name: 'КОШАКА',
+  speak: speak,
+};
+
+console.log(dog.speak());
